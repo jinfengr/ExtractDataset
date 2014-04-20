993c838ac807d30d22b08a1c74e500acf57ece91
Jeffyrao <raojinfeng@gmail.com>
2014-04-20 12:52:03 -0400

    add BuildFeature.py

--- src/BuildFeatures.py
+++ src/BuildFeatures.py
@@ -9,23 +9,48 @@ from collections import defaultdict
 
 def ExtractFeatures(paragraph, dataset,features):
     sentences = paragraph.split(' . ')
-    for s in sentences:
-        if s.find(dataset)>=0:
-            words = s.split(' ')
+    for i in range(len(sentences)):
+        s = sentences[i]
+        nexts = ""
+        if i < len(sentences)-1:
+            nexts = sentences[i+1]
+        words = s.split(' ')
+        names = dataset.split(' ')
+        flag = True
+        for name in names:
+            if name not in words:
+                flag = False
+        if flag == True:
+            words.extend(nexts.split(' '))
             for word in words:
                 if keywords.has_key(word):
-                    dist = abs(words.index(word) - words.index(dataset))
+                    dist = abs(words.index(word) - words.index(names[0]))
                     index = keywords.keys().index(word)
-                    features[dataset] = defaultdict(int)
                     features[dataset][2*index] += 1
                     features[dataset][2*index+1] += dist
-    
+            flag = False
+                
+
+def SaveFeatures(features):
+    f = open(data_dir+"features.dat","w")
+    for dataset in features:
+        f.write(dataset+":")
+        for index in range(COUNT):
+            freq = features[dataset][2*index]
+            if freq==0:
+                dist = 0
+            else:
+                dist = features[dataset][2*index+1]/freq
+            f.write(str(freq) +","+ str(dist)+",")
+        f.write("\n")
+    f.close()
+         
 data_dir = '../data/training_data/'
 
-COUNT = 1000
+COUNT = 500
 count = 0
 keywords = {}
-with open(data_dir+'keywords.txt') as f:
+with open(data_dir+'keywords.dat') as f:
     for line in f:
         if count <= COUNT:
             tokens = line.rstrip('\n').split(' ')
@@ -33,20 +58,22 @@ with open(data_dir+'keywords.txt') as f:
             count += 1
 
 datasets = {}
-with open(data_dir+'dataset.txt') as f:
+features = {}
+with open(data_dir+'dataset.dat') as f:
     for line in f:
         sets = line.rstrip('\n').split(',')
         datasets[sets[0]]=sets[1:]
 
 files = os.listdir(data_dir)
-features = {}
 for f in files:
     if f.endswith('_dataset.txt'):
         infp = open(data_dir+f,'rb')
         data = datasets[f[0:-12]]
+        for dataset in data:
+                features[dataset] = defaultdict(int)
         for para in infp:
             [ExtractFeatures(para,dataset,features) for dataset in data]
                     
-                    
+SaveFeatures(features)                    
                 
                 
--- src/ExtractWords.py
+++ src/ExtractWords.py
@@ -10,23 +10,34 @@ from nltk.tokenize import RegexpTokenizer
 
 data_dir = '../data/training_data/'
 
+datasets = {}
+with open(data_dir+'dataset.dat') as f:
+    for line in f:
+        sets = line.rstrip('\n').split(',')
+        datasets[sets[0]]=sets[1:]
+
 words_freq = {}
 files = os.listdir(data_dir)
 tokenizer = RegexpTokenizer("[a-zA-Z-]+", flags=re.UNICODE)
 for f in files:
     if f.endswith('_dataset.txt'):
         infp = open(data_dir+f,'rb')
-        for line in infp:
-            tokens = tokenizer.tokenize(line.encode('UTF_8'))
-            tokens = [stem(token.lower()) for token in tokens]
-            for word in tokens:
-                if words_freq.has_key(word):
-                    words_freq[word] += 1
-                else:
-                    words_freq[word] = 1
+        data = datasets[f[0:-12]]
+        for para in infp:
+            sentences = para.split(' . ')
+            for s in sentences:
+                for dataset in data:
+                    if s.find(dataset)>=0:
+                        tokens = tokenizer.tokenize(s.encode('UTF_8'))
+                        tokens = [stem(token.lower()) for token in tokens]
+                        for word in tokens:
+                            if words_freq.has_key(word):
+                                words_freq[word] += 1
+                            else:
+                                words_freq[word] = 1
 
 words_freq = sorted(words_freq.items(),key=lambda x: x[1], reverse = True)
-outfp = file(data_dir+'keywords.txt','w')
+outfp = file(data_dir+'keywords.dat','w')
 for word in words_freq:
     outfp.write(word[0]+' '+str(word[1])+'\n')
 outfp.close()
